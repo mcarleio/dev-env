42ef801e0c81a5ebf4b5526b7378bad743db79c7
Marcel Carlé <hi-github@mcarle.io>
2022-02-01 23:11:59 +0100

    fix: set wallpaper only when download successful

--- scripts/reload-wallpaper.sh
+++ scripts/reload-wallpaper.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
+function reload_wallpaper() {
+    # Load random wallpaper: https://wallhaven.cc/help/api
+    WALLPAPER_URL=`curl -s "https://wallhaven.cc/api/v1/search?ratios=${DIMENSION_WIDTH}x${DIMENSION_HEIGHT}&atleast=${DIMENSION_WIDTH}x${DIMENSION_HEIGHT}&sorting=random" | jq -r '.data[0].path'`
+
+    if [[ -n "$WALLPAPER_URL" ]]; then
+
+        # Download it
+        wget -q -nc $WALLPAPER_URL -P /opt/.wallpaper/ &>/dev/null
+
+        # Get its name
+        WALLPAPER_NAME=`basename $WALLPAPER_URL`
+
+        # Set it as wallpaper and screensaver/lockscreen
+        gsettings set org.gnome.desktop.background picture-uri "file:///opt/.wallpaper/$WALLPAPER_NAME"
+        gsettings set org.gnome.desktop.screensaver picture-uri "file:///opt/.wallpaper/$WALLPAPER_NAME"
+    fi
+}
+
 # Determine primary screen resolution
 DIMENSION_WIDTH=`xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1`
 DIMENSION_HEIGHT=`xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2`
 
-# Load random wallpaper: https://wallhaven.cc/help/api
-WALLPAPER_URL=`curl -s "https://wallhaven.cc/api/v1/search?ratios=${DIMENSION_WIDTH}x${DIMENSION_HEIGHT}&atleast=${DIMENSION_WIDTH}x${DIMENSION_HEIGHT}&sorting=random" | jq -r '.data[0].path'`
+reload_wallpaper;
 
-# Download it
-wget -q -nc $WALLPAPER_URL -P /opt/.wallpaper/ &>/dev/null
+if [[ -z "$WALLPAPER_URL" ]]; then
 
-# Get its name
-WALLPAPER_NAME=`basename $WALLPAPER_URL`
+    sleep 5;
+    reload_wallpaper;
 
-# Set it as wallpaper and screensaver/lockscreen
-gsettings set org.gnome.desktop.background picture-uri "file:///opt/.wallpaper/$WALLPAPER_NAME"
-gsettings set org.gnome.desktop.screensaver picture-uri "file:///opt/.wallpaper/$WALLPAPER_NAME"
+fi
